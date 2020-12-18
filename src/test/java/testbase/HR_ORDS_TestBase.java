bd7508c61ea0dfd2ba020e3234588c7d1404a07c
sevdaTan <sevdatanyildizi@gmail.com>
2020-12-17 20:40:23 -0500

    HR_ORDS_TestBase.java is updated

--- src/test/java/testbase/HR_ORDS_TestBase.java
+++ src/test/java/testbase/HR_ORDS_TestBase.java
@@ -2,21 +2,26 @@ package testbase;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-
+import Utility.ConfigurationReader;
+import Utility.DB_Utility;
 import static io.restassured.RestAssured.*;
-
 public abstract class HR_ORDS_TestBase {
-
-
     @BeforeAll
     public static void setUp(){
-        baseURI = "http://54.90.101.103:1000";
-        basePath = "/ords/hr" ;
+        baseURI  = ConfigurationReader.getProperty("ords.baseURL");
+        basePath = ConfigurationReader.getProperty("ords.basePath");
+        // create DB Connection here
+        DB_Utility.createConnection( ConfigurationReader.getProperty("hr.database.url"),
+                ConfigurationReader.getProperty("hr.database.username"),
+                ConfigurationReader.getProperty("hr.database.password")
+        );
     }
-
     @AfterAll
     public static void tearDown(){
         reset();
+        // Destroy DB Connection here
+        DB_Utility.destroy();
     }
-
 }
+
+
