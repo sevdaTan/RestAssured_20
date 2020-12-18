a5284c11dbaa82089dc5a238073cc05171535bdd
sevdaTan <sevdatanyildizi@gmail.com>
2020-12-17 19:45:00 -0500

    updating HR_ORDS_Test.java

--- src/test/java/day8/HR_ORDS_Test.java
+++ src/test/java/day8/HR_ORDS_Test.java
@@ -5,12 +5,15 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
 import pojo.BookCategory;
+import pojo.Country;
 import pojo.Region;
 import java.util.List;
 import java.util.Map;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
+
+
 public class HR_ORDS_Test {
 
     //http://54.90.101.103:1000/ords/hr/countries/AR
@@ -29,14 +32,28 @@ public class HR_ORDS_Test {
     @Test
     public void testCountryResponseToPOJO(){
 
+        //Response response =  get("/countries/{country_id}", "AR").prettyPeek();
+        Response response = given()
+                .pathParam("country_id","AR").
+                        when()
+                .get("/countries/{country_id}").prettyPeek();
+        Country ar = response.as(Country.class) ;
+        System.out.println("Argentina = " + ar);
 
-
+        Country ar1 = response.jsonPath().getObject("",Country.class);
+        System.out.println("Argentina with jsonPath = " + ar1);
 
 
     }
+    @DisplayName("Test GET /countries to List of POJO")
+    @Test
+    public void testAllCountriesResponseToListOfPOJO(){
 
+        Response response = get("/countries").prettyPeek() ;
+        List<Country> countryList = response.jsonPath().getList("items", Country.class) ;
+        countryList.forEach(System.out::println);
 
-
+    }
 
 
 
