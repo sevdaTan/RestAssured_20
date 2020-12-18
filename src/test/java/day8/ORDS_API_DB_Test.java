1fa9ec9d3d3b1e35e75faf525e262b97b6bca0e3
sevdaTan <sevdatanyildizi@gmail.com>
2020-12-17 21:47:17 -0500

    Comparing GET /regions/{region_id} to Database result MAP TO MAP

--- src/test/java/day8/ORDS_API_DB_Test.java
+++ src/test/java/day8/ORDS_API_DB_Test.java
@@ -1,5 +1,6 @@
 package day8;
 
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pojo.Region;
@@ -22,11 +23,12 @@ public class ORDS_API_DB_Test extends HR_ORDS_TestBase {
 
     @DisplayName("Testing the connection with query")
     @Test
-    public void testDB_Connection(){
+    public void testDB_Connection() {
 
         DB_Utility.runQuery("SELECT * FROM REGIONS");
         DB_Utility.displayAllData();
     }
+
     /**
      * Send an GET /regions/{region_id} request with region_id of 3
      * check status code is 200
@@ -39,9 +41,9 @@ public class ORDS_API_DB_Test extends HR_ORDS_TestBase {
 
     @DisplayName("Testing GET /regions/{region_id} Data Match Database Data")
     @Test
-    public void testRegionDataFromResponseMatchDB_Data(){
+    public void testRegionDataFromResponseMatchDB_Data() {
 
-        int myID = 3 ;
+        int myID = 3;
         Response response = given()
                 .pathParam("region_id", myID).
                         when()
@@ -52,23 +54,61 @@ public class ORDS_API_DB_Test extends HR_ORDS_TestBase {
                 .extract()
                 .response();
 
-        Region r3 = response.as(Region.class) ;
+        Region r3 = response.as(Region.class);
         System.out.println("r3 = " + r3);
+
+        DB_Utility.runQuery("SELECT * FROM REGIONS WHERE REGION_ID = " + myID);
+        Map<String, String> expectedResultMap = DB_Utility.getRowMap(1);
+
+        System.out.println("expectedResultMap = " + expectedResultMap);
+
+        // verify the actual result from api response match expected database result
+        assertThat(r3.getRegion_id() + "", equalTo(expectedResultMap.get("REGION_ID")));
+        assertThat(r3.getRegion_name(), is(expectedResultMap.get("REGION_NAME")));
+
+    }
+
+    @DisplayName("Testing GET /regions/{region_id} Data Match Database Data With Both Maps")
+    @Test
+    public void testRegionDataFromResponseMatchDB_Data2() {
+
+        int myID = 3;
+        JsonPath jp = given()
+                .pathParam("region_id", myID).
+                        when()
+                .get("/regions/{region_id}").
+                        then()
+                .log().body()
+                .statusCode(200)
+                .extract()
+                .jsonPath();
+
+        // save the response json as a Map object
+        // Here we are calling the overloaded version of getMap method with 3 params
+        // 1. jsonPath String
+        // 2. Data type Map key
+        // 3. Data type Map value
+        // so we can make sure we get exactly what we asked for
+        Map<String, String> actualResultMap = jp.getMap("",String.class,String.class);
+        // do not need to remove extra links from json result
+        // because we are checking key value pair , anything we dont check will not matter
+        System.out.println("actualResultMap = " + actualResultMap);
 
         DB_Utility.runQuery("SELECT * FROM REGIONS WHERE REGION_ID = "+myID ) ;
         Map<String,String> expectedResultMap =  DB_Utility.getRowMap(1) ;
 
         System.out.println("expectedResultMap = " + expectedResultMap);
 
-        // verify the actual result from api response match expected database result
-        assertThat(r3.getRegion_id()+"" ,  equalTo(expectedResultMap.get("REGION_ID") )  );
-        assertThat(r3.getRegion_name()  , is(expectedResultMap.get("REGION_NAME"))  );
+        // since the keyname is different in both map we can not directly
+        // compare map to map object
+        // we have to compare the value of key step by step
+        assertThat(  actualResultMap.get("region_id") ,
+                equalTo( expectedResultMap.get("REGION_ID") ) );
+        assertThat(actualResultMap.get("region_name") ,
+                equalTo( expectedResultMap.get("REGION_NAME") ) );
+
+
 
     }
-
-
-
-
-
 
 }
