c25d320e8ebb2e086f66b70e1afb3c79deca910b
sevdaTan <sevdatanyildizi@gmail.com>
2020-12-19 16:17:34 -0500

    parameterized test

--- src/test/java/day9/Junit5_ParameterizedTest.java
+++ src/test/java/day9/Junit5_ParameterizedTest.java
@@ -1,8 +1,11 @@
 package day9;
 
-
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class Junit5_ParameterizedTest {
@@ -18,6 +21,31 @@ public class Junit5_ParameterizedTest {
 
     }
     // CSV file -->> Comma Separated Values
-    //  using CSV file
+    //  using CSV file as source for parameterized test
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/zipcode.csv" , numLinesToSkip = 1)
+    public void test2(String zip){
+        System.out.println("zip = "+ zip);
+        //sending request to zipcode endpoint here
+
+        //api.zippopotam.us/us/90210
+        // baseurl =api.zippopotam.us
+        // endpoint is /us/{zipcode}
+
+       given()
+       .baseUri("https://api.zippopotam.us")
+       .pathParam("zipcode" , 10000).
+               when()
+               .get("/us/{zipcode}").
+               then()
+               .statusCode(200) ;
+
+
+
+    }
+
+
+
 
 }
