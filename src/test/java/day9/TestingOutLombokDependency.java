dff7b30c6e7a8bd9bdd86ff99397a698dd5b3fa0
sevdaTan <sevdatanyildizi@gmail.com>
2020-12-19 12:41:11 -0500

    Practice De-serializing into List of Pojo and removeIf method of list

--- src/test/java/day9/TestingOutLombokDependency.java
+++ src/test/java/day9/TestingOutLombokDependency.java
@@ -1,9 +1,18 @@
 package day9;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pojo.Department;
+import testbase.HR_ORDS_TestBase;
 
-public class TestingOutLombokDependency {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestingOutLombokDependency extends HR_ORDS_TestBase {
 
 @Test
     public void testDepartmentPOJO(){
@@ -16,11 +25,25 @@ public class TestingOutLombokDependency {
             = new Department(100,"ABC",12,1700);
     System.out.println("d2 = " + d2);
 
-
-
 }
+    @DisplayName("GET /departments and save List of POJO")
+    @Test
+    public void testDepartmentJsonArrayToListOfPojo(){
+        List<Department> allDeps = get("/departments")
+                .jsonPath().getList("items", Department.class) ;
+        //allDeps.forEach(System.out::println);
+        // COPY THE CONTENT OF THIS LIST INTO NEW LIST
+        // AND ONLY PRINT IF THE DEP MANAGER ID IS NOT NULL
+        List<Department> allDepsCopy = new ArrayList<>(allDeps);
+        allDepsCopy.removeIf( eachDep -> eachDep.getManager_id()==0 ) ;
+        allDepsCopy.forEach(System.out::println);
+    }
 
-
+//    @DisplayName("GET /departments and filter the result with JsonPath groovy")
+//    @Test
+//    public void testFilterResultWithGroovy(){
+//        JsonPath jp = get("/departments").jsonPath();
+//    }
 
 
 }
