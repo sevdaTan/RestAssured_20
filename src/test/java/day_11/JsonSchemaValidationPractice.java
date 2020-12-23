83860e0ed41707c8320e3bf9c1a71711fffc0d2d
sevdaTan <sevdatanyildizi@gmail.com>
2020-12-22 23:39:56 -0500

    updated

--- src/test/java/day_11/JsonSchemaValidationPractice.java
+++ src/test/java/day_11/JsonSchemaValidationPractice.java
@@ -1,9 +1,67 @@
 package day_11;
 
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import testbase.SpartanAdminTestBase;
+import utility.SpartanUtil;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 
-public class JsonSchemaValidationPractice {
+public class JsonSchemaValidationPractice extends SpartanAdminTestBase {
 
+    @DisplayName("Testing GET /spartans endpoint structure")
+    @Test
+    public void testAllSpartanResponseSchema(){
+        given()
+                .spec(adminReqSpec).
+        when()
+                .get("/spartans").
+        then()
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("allSpartansSchema.json")) ;
+    }
+
+    @DisplayName("Testing POST /spartans endpoint response structure")
+    @Test
+    public void testPostSpartanResponseSchema(){
+
+        // We can also use matchesJsonSchema method if we want to provide full path for this file
+        File schemaFile = new File("src/test/resources/postSuccessResponseSchema.json");
+
+        given()
+                .spec(adminReqSpec)
+                .contentType(ContentType.JSON)
+                .body(SpartanUtil.getRandomSpartanPOJO_Payload()).
+        when()
+                .post("/spartans").
+        then()
+                //.body(matchesJsonSchemaInClasspath("postSuccessResponseSchema.json") )
+                // what if my schema file is somewhere else other than resource folder ?
+                // then you need t provide full path and use different method
+                .body(matchesJsonSchema( schemaFile )  )
+        ;
+    }
+    @DisplayName("Testing GET /spartans/search endpoint response structure")
+    @Test
+    public void testSearchSpartanResponseSchema(){
+        given()
+                .spec(adminReqSpec)
+                .queryParam("nameContains","a")
+                .queryParam("gender","Female").
+        when()
+                .get("/spartans/search").
+        then()
+                .body(matchesJsonSchemaInClasspath("searchSpartanSchema.json") )
+        ;
+
+
+    }
 
 
 }
